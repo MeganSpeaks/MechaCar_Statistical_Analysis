84a34f423a409bac47df9be8d892ae2a04ca0a39
MeganSpeaks <86204809+MeganSpeaks@users.noreply.github.com>
2021-11-14 21:39:00 -0600

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,3 +6,5 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 Suspension_Coil <- read.csv(file = 'Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))= sd(PSI))
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+t.test(Suspension_Coil$PSI, mu = 1500)
